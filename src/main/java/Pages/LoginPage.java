000eee21fe558f1bafb4adf49917a58927ea1fcc
nazneen.rahman <itsnazn33n@gmail.com>
2024-03-18 10:40:47 +0600

    Initial commit

---
+++ src/main/java/Pages/LoginPage.java
@@ -0,0 +1,30 @@
+package Pages;
+
+import Base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends TestBase {
+    //Page Factory - OR:
+    @FindBy(name = "email")
+    WebElement email;
+    @FindBy(name = "password")
+    WebElement password;
+    @FindBy(xpath = "/html/body/div[1]/div/div/form/div/div[3]")
+    WebElement loginButton;
+
+    public LoginPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public DashboardPage login(String email, String password) {
+        this.email.sendKeys(email);
+        this.password.sendKeys(password);
+        loginButton.click();
+        return new DashboardPage();
+
+
+    }
+
+}
---
+++ src/test/java/Tests/LoginPageTestCases.java
@@ -0,0 +1,36 @@
+package Tests;
+
+import Base.TestBase;
+import Pages.DashboardPage;
+import Pages.LoginPage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginPageTestCases extends TestBase {
+    LoginPage loginPage;
+    DashboardPage dashboard;
+
+    public LoginPageTestCases() {
+        super();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        loginPage = new LoginPage();
+    }
+
+    @Test
+    public void loginTest() {
+
+        dashboard = loginPage.login(prop.getProperty("email"), prop.getProperty("password"));
+
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
